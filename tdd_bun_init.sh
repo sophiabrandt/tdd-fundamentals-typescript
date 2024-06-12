ddda7f8fc937b0509573db4d51c2643886964e9d
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2024-06-12 18:42:44 +0200

    chore: add initialization script for new katas

---
+++ tdd_bun_init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+bun init -y
+mkdir src
+mkdir test
+
+bun add -d typescript @types/bun jest ts-jest @types/jest
+
+# Use jq to update the package.json file
+jq '. + {scripts: {"test": "jest", "test:w": "jest --watchAll"}}' package.json > temp_package.json && mv temp_package.json package.json
+
